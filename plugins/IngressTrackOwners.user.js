ad9bfc617db29fd538ff44c6ee50c4eb60fec3e8
Felipe Dewulf <felipedewulf@gmail.com>
2015-12-12 20:59:07 -0200

    Adding new plugin

---
+++ plugins/IngressTrackOwners.user.js
@@ -0,0 +1,49 @@
+﻿// ==UserScript==
+// @id             iitc-plugin-trackowners
+// @name           IITC plugin: Track Portal Owners
+// @category       Misc
+// @version        0.0.1.@@DATETIMEVERSION@@
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      @@UPDATEURL@@
+// @downloadURL    @@DOWNLOADURL@@
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Allow tracking of a region portal ownership. 
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
+
+function wrapper(plugin_info) {
+// ensure plugin framework is there, even if iitc is not yet loaded
+if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
+//(leaving them in place might break the 'About IITC' page or break update checks)
+plugin_info.buildName = 'TrackOwners';
+plugin_info.dateTimeVersion = '201512087.154202';
+plugin_info.pluginId = 'PortalTrackOwners';
+//END PLUGIN AUTHORS NOTE
+
+//PLUGIN START ////////////////////////////////////////////////////////
+
+//PLUGIN END //////////////////////////////////////////////////////////
+
+
+
+setup.info = plugin_info; //add the script info data to the function as a property
+if(!window.bootPlugins) window.bootPlugins = [];
+window.bootPlugins.push(setup);
+// if IITC has already booted, immediately run the 'setup' function
+if(window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+// inject code into site context
+var script = document.createElement('script');
+var info = {};
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
+(document.body || document.head || document.documentElement).appendChild(script);
+
+
+
+
+
